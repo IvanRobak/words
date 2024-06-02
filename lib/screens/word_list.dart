295405d8bfbe9c0e483d0c667ab22dbc4fe2e917
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-02 18:49:10 +0300

    condition for wordList

--- lib/screens/word_list.dart
+++ lib/screens/word_list.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:words/services/word_loader.dart';
 import 'package:words/widgets/word_button.dart';
+import 'package:words/services/word_loader.dart';
 import '../models/word.dart';
 
 class WordListScreen extends StatefulWidget {
-  const WordListScreen({super.key});
+  const WordListScreen({Key? key}) : super(key: key);
 
   @override
   State<WordListScreen> createState() => _WordListScreenState();
@@ -12,6 +12,8 @@ class WordListScreen extends StatefulWidget {
 
 class _WordListScreenState extends State<WordListScreen> {
   List<Word> words = [];
+  int columns = 2; // Поточна кількість стовпців
+  final List<int> columnOptions = [2, 3]; // Можливі варіанти
 
   @override
   void initState() {
@@ -28,27 +30,61 @@ class _WordListScreenState extends State<WordListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Налаштування відстані між картками в залежності від кількості стовпців
+    final double mainAxisSpacing = columns == 2 ? 10 : 4;
+    final double crossAxisSpacing = columns == 2 ? 10 : 4;
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('All words'),
-        ),
-        body: words.isEmpty
-            ? const Center(child: CircularProgressIndicator())
-            : Padding(
-                padding: const EdgeInsets.all(16),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, // Два стовпці
-                    mainAxisSpacing: 10, // Відстань між рядами
-                    crossAxisSpacing: 10, // Відстань між стовпцями
-                    childAspectRatio: 3, // Співвідношення сторін елемента
-                  ),
-                  itemCount: words.length,
-                  itemBuilder: (context, index) {
-                    final word = words[index];
-                    return WordButton(word: word);
+      appBar: AppBar(
+        title: const Text('All words'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 26),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropdownButton<int>(
+                  value: columns,
+                  items: columnOptions.map((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                  onChanged: (int? newValue) {
+                    setState(() {
+                      columns = newValue!;
+                    });
                   },
                 ),
-              ));
+              ],
+            ),
+          ),
+          Expanded(
+            child: words.isEmpty
+                ? const Center(child: CircularProgressIndicator())
+                : Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: columns, // Використовуйте змінну
+                        mainAxisSpacing: mainAxisSpacing, // Відстань між рядами
+                        crossAxisSpacing:
+                            crossAxisSpacing, // Відстань між стовпцями
+                        childAspectRatio: 3, // Співвідношення сторін елемента
+                      ),
+                      itemCount: words.length,
+                      itemBuilder: (context, index) {
+                        final word = words[index];
+                        return WordButton(word: word, columns: columns);
+                      },
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/word_button.dart
+++ lib/widgets/word_button.dart
@@ -3,12 +3,18 @@ import 'package:words/models/word.dart';
 import 'package:words/screens/word_details.dart';
 
 class WordButton extends StatelessWidget {
-  const WordButton({super.key, required this.word});
+  const WordButton({super.key, required this.word, required this.columns});
 
   final Word word;
+  final int columns;
 
   @override
   Widget build(BuildContext context) {
+    // Налаштування стилю в залежності від кількості стовпців
+    final double fontSize = columns == 2 ? 16 : 14;
+    final double verticalPadding = columns == 2 ? 8 : 3;
+    final double horizontalPadding = columns == 2 ? 12 : 5;
+
     return Padding(
         padding: const EdgeInsets.all(5),
         child: ElevatedButton(
@@ -25,14 +31,14 @@ class WordButton extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
-            padding: const EdgeInsets.symmetric(
-                vertical: 8, horizontal: 12), // Зменште ці значення
-            textStyle: const TextStyle(fontSize: 16),
+            padding: EdgeInsets.symmetric(
+                vertical: verticalPadding, horizontal: horizontalPadding),
+            textStyle: TextStyle(fontSize: fontSize),
           ),
           child: Text(
             word.word,
-            style: const TextStyle(
-              fontSize: 16,
+            style: TextStyle(
+              fontSize: fontSize,
               color: Colors.white,
             ),
           ),
