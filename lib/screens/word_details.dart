0d8d886b29f2e1637b140071ab80cf0fa51056f8
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-03 18:11:57 +0300

    style for WordDetailScreen

--- lib/screens/word_details.dart
+++ lib/screens/word_details.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:words/models/word.dart';
 
-class WordDetailScreen extends StatelessWidget {
+class WordDetailScreen extends StatefulWidget {
   final Word word;
 
   const WordDetailScreen({super.key, required this.word});
+
+  @override
+  State<WordDetailScreen> createState() => _WordDetailScreenState();
+}
+
+class _WordDetailScreenState extends State<WordDetailScreen> {
+  final TextEditingController _textController = TextEditingController();
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.addListener(_handleTyping);
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _handleTyping() {
+    setState(() {
+      _isTyping = _textController.text.isNotEmpty;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,37 +43,88 @@ class WordDetailScreen extends StatelessWidget {
         child: Card(
           color: const Color(0xFF426CD8),
           margin: const EdgeInsets.all(20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
           child: SingleChildScrollView(
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  const SizedBox(
-                    height: 36,
+                  const SizedBox(height: 36),
+                  Center(
+                    child: Text(
+                      widget.word.word,
+                      style: const TextStyle(color: Colors.white, fontSize: 35),
+                    ),
                   ),
-                  Text(
-                    word.word,
-                    style: const TextStyle(color: Colors.white, fontSize: 35),
+                  const SizedBox(height: 16),
+                  Center(
+                    child: Text(
+                      widget.word.description,
+                      style: const TextStyle(color: Colors.white, fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Text(
-                    word.description,
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                  const Column(
-                    children: [
-                      TextField(),
-                      SizedBox(
-                        height: 20,
+                  const SizedBox(height: 30),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'My ',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          TextSpan(
+                            text: widget.word.word,
+                            style: const TextStyle(
+                                color: Color.fromARGB(255, 165, 38, 29),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const TextSpan(
+                            text: ' is painting.',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ],
                       ),
-                      TextField(),
-                    ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 80,
+                  const SizedBox(height: 20),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Row(
+                      children: [
+                        if (!_isTyping)
+                          const Icon(Icons.add, color: Colors.white),
+                        if (!_isTyping) const SizedBox(width: 10),
+                        Expanded(
+                          child: TextField(
+                            controller: _textController,
+                            decoration: const InputDecoration(
+                              hintText: 'Add your own example',
+                              border: InputBorder.none,
+                              hintStyle: TextStyle(color: Colors.white),
+                            ),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  const SizedBox(height: 80),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -59,9 +136,16 @@ class WordDetailScreen extends StatelessWidget {
                         ),
                       ),
                       ElevatedButton(
-                          onPressed: () {}, child: const Text('Hobby'))
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                        child: const Text('Hobby'),
+                      ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
