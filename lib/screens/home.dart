5112470d1f302853bdc08c5685ddda3c438d2c23
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-06 16:09:20 +0300

    style for home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:words/screens/folder_list.dart';
 import 'package:words/screens/word_list.dart';
+// import 'package:words/screens/categories_screen.dart'; // Імпортуйте екран з категоріями, коли він буде створений
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -9,79 +10,112 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      backgroundColor: const Color(0xFFFFF3EA),
       appBar: AppBar(
-        title: const Text('Discover words'),
+        backgroundColor: const Color(0xFFFFF3EA),
+        elevation: 0,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // crossAxisAlignment: CrossAxisAlignment.,
             children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const WordListScreen(),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 30),
+                child: Center(
+                  child: Text(
+                    'Discover\nwords',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF426CD8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
                   ),
-                  padding: const EdgeInsets.symmetric(vertical: 15),
                 ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        'All words',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 21,
-                        ),
+              ),
+              SizedBox(
+                width: 250, // Задайте бажану ширину кнопки
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WordListScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 65, 93, 104),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 25),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'All words',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const FolderListScreen(),
+              SizedBox(
+                width: 250, // Задайте бажану ширину кнопки
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 65, 93, 104),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
                     ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF426CD8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                    padding: const EdgeInsets.symmetric(vertical: 25),
                   ),
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        'My folders',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 21,
-                        ),
+                  child: const Center(
+                    child: Text(
+                      'Categories',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
                       ),
                     ),
-                  ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 250, // Задайте бажану ширину кнопки
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const FolderListScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 65, 93, 104),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 25),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'My folders',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
