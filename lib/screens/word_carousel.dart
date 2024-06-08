c2e286052438902d669733c13ecf01380468fcef
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-08 22:52:20 +0300

    arrows for carousel

--- lib/screens/word_carousel.dart
+++ lib/screens/word_carousel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:words/models/word.dart';
 import 'package:words/widgets/word_detail.dart';
 
-class WordCarouselScreen extends StatelessWidget {
+class WordCarouselScreen extends StatefulWidget {
   final List<Word> words;
   final int initialIndex;
 
@@ -10,25 +10,90 @@ class WordCarouselScreen extends StatelessWidget {
       {super.key, required this.words, required this.initialIndex});
 
   @override
+  _WordCarouselScreenState createState() => _WordCarouselScreenState();
+}
+
+class _WordCarouselScreenState extends State<WordCarouselScreen> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: widget.initialIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Word Carousel'),
       ),
-      body: PageView.builder(
-        controller: PageController(initialPage: initialIndex),
-        itemCount: words.length,
-        itemBuilder: (context, index) {
-          final word = words[index];
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 160),
-            child: SizedBox(
-              height:
-                  MediaQuery.of(context).size.height * 0.7, // Обмеження висоти
-              child: WordDetail(word: word),
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: widget.words.length,
+            itemBuilder: (context, index) {
+              final word = widget.words[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 160),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height *
+                      0.7, // Обмеження висоти
+                  child: WordDetail(word: word),
+                ),
+              );
+            },
+          ),
+          Positioned(
+            bottom: 20,
+            left: 80,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  if (_pageController.page! > 0) {
+                    _pageController.previousPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+              ),
             ),
-          );
-        },
+          ),
+          Positioned(
+            bottom: 20,
+            right: 80,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_forward, color: Colors.white),
+                onPressed: () {
+                  if (_pageController.page! < widget.words.length - 1) {
+                    _pageController.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
