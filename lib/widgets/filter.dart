19b0c14194c8fcfa211fd3df265e5341eea3d48e
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-11 21:07:54 +0300

    icons for FilterBar

--- lib/widgets/filter.dart
+++ lib/widgets/filter.dart
@@ -20,25 +20,37 @@ class FilterBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 20),
+      padding: const EdgeInsets.only(left: 5, right: 5, bottom: 20),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          DropdownButton<int>(
-            value: columns,
-            items: columnOptions.map((int value) {
-              return DropdownMenuItem<int>(
-                value: value,
-                child: Text(
-                  value.toString(),
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.secondary),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.grid_view,
+                  color: columns == 2
+                      ? Theme.of(context).colorScheme.secondary
+                      : Colors.grey,
                 ),
-              );
-            }).toList(),
-            onChanged: onColumnsChanged,
+                onPressed: () {
+                  onColumnsChanged(2);
+                },
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.view_module,
+                  color: columns == 3
+                      ? Theme.of(context).colorScheme.secondary
+                      : Colors.grey,
+                ),
+                onPressed: () {
+                  onColumnsChanged(3);
+                },
+              ),
+            ],
           ),
-          const SizedBox(width: 120),
+          const SizedBox(width: 50),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.only(left: 16),
