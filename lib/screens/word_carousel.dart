22e4179b52cc2f292ae2f02a384df81a24731f9f
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-22 12:28:04 +0300

    grid for carousel

--- lib/screens/word_carousel.dart
+++ lib/screens/word_carousel.dart
@@ -10,21 +10,31 @@ class WordCarouselScreen extends StatefulWidget {
       {super.key, required this.words, required this.initialIndex});
 
   @override
-  // ignore: library_private_types_in_public_api
   _WordCarouselScreenState createState() => _WordCarouselScreenState();
 }
 
 class _WordCarouselScreenState extends State<WordCarouselScreen> {
   late PageController _pageController;
+  int _currentPageIndex = 0;
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController(
       initialPage: widget.initialIndex,
-      viewportFraction:
-          0.95, // Змінити частину екрана, яку займає кожна сторінка
+      viewportFraction: 0.95,
     );
+
+    _currentPageIndex = widget.initialIndex;
+
+    _pageController.addListener(() {
+      final nextPage = _pageController.page?.round();
+      if (nextPage != null && nextPage != _currentPageIndex) {
+        setState(() {
+          _currentPageIndex = nextPage;
+        });
+      }
+    });
   }
 
   @override
@@ -49,56 +59,47 @@ class _WordCarouselScreenState extends State<WordCarouselScreen> {
               return Padding(
                 padding: const EdgeInsets.only(bottom: 70),
                 child: SizedBox(
-                  height: MediaQuery.of(context).size.height *
-                      0.7, // Обмеження висоти
+                  height: MediaQuery.of(context).size.height * 0.7,
                   child: WordDetail(word: word),
                 ),
               );
             },
           ),
           Positioned(
-            bottom: 20,
-            left: 80,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.4),
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () {
-                  if (_pageController.page! > 0) {
-                    _pageController.previousPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  }
-                },
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 80,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.4),
-                shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.arrow_forward, color: Colors.white),
-                onPressed: () {
-                  if (_pageController.page! < widget.words.length - 1) {
-                    _pageController.nextPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                    );
-                  }
-                },
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: SizedBox(
+                height: 80,
+                width: 220,
+                child: GridView.builder(
+                  itemCount: 50, // П'ять рядів по десять квадратів
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 10, // 10 квадратів в рядку
+                    mainAxisSpacing: 2, // Відстань між рядками
+                    crossAxisSpacing: 1, // Відстань між стовпцями
+                    childAspectRatio: 1.6, // Співвідношення сторін елементів
+                  ),
+                  itemBuilder: (context, index) {
+                    return buildDot(index, context);
+                  },
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget buildDot(int index, BuildContext context) {
+    return Container(
+      // height: 5,
+      // width: 5,
+      decoration: BoxDecoration(
+        color: _currentPageIndex == index ? Colors.blue : Colors.grey,
+        shape: BoxShape.circle,
       ),
     );
   }
