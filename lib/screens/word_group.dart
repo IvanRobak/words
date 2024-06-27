af460d6e3f9905c3ab9436e583b7b2ee9ae82fe2
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-06-27 21:46:09 +0300

    WordGroupScreen refactoring

--- lib/screens/word_group.dart
+++ lib/screens/word_group.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:words/providers/button_provider.dart';
 import 'package:words/providers/word_provider.dart';
-import 'package:words/models/word.dart';
 import 'package:words/screens/word_carousel.dart';
 import 'package:words/services/word_loader.dart';
+import 'package:words/widgets/dot_builder.dart';
 
 class WordGroupScreen extends ConsumerStatefulWidget {
   const WordGroupScreen({super.key});
@@ -27,6 +28,8 @@ class _WordGroupScreenState extends ConsumerState<WordGroupScreen> {
   @override
   Widget build(BuildContext context) {
     final words = ref.watch(wordFilterProvider);
+    final knownWords = ref.watch(knownWordsProvider);
+    final learnWords = ref.watch(learnWordsProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -34,27 +37,17 @@ class _WordGroupScreenState extends ConsumerState<WordGroupScreen> {
         title: Text('All words',
             style: TextStyle(color: Theme.of(context).colorScheme.onSecondary)),
         iconTheme: IconThemeData(
-          color: Theme.of(context)
-              .colorScheme
-              .onSecondary, // Задаємо колір для значків
+          color: Theme.of(context).colorScheme.onSecondary,
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, // Кількість стовпців у рядку
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            childAspectRatio: 1, // Співвідношення сторін елементів
-          ),
-          itemCount: 10, // Кількість квадратиків
+        child: ListView.builder(
+          itemCount: (words.length / 50).ceil(),
           itemBuilder: (context, index) {
             final start = index * 50;
             final end = start + 50;
-            words.length > start
-                ? words.sublist(start, end > words.length ? words.length : end)
-                : <Word>[];
+            words.sublist(start, end > words.length ? words.length : end);
 
             return GestureDetector(
               onTap: () {
@@ -62,24 +55,62 @@ class _WordGroupScreenState extends ConsumerState<WordGroupScreen> {
                   context,
                   MaterialPageRoute(
                     builder: (context) => WordCarouselScreen(
-                      words: words, // Передаємо всі слова
+                      words: words,
                       initialIndex: start,
                     ),
                   ),
                 );
               },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Center(
-                  child: Text(
-                    '$start - $end',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
+              child: Card(
+                elevation: 4.0,
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 90,
+                        alignment: Alignment.center,
+                        child: Text(
+                          '$start - $end',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSecondary,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: 50,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 10, // 10 кружечків в рядку
+                            mainAxisSpacing: 4.0,
+                            crossAxisSpacing: 8.0,
+                            childAspectRatio: 1, // Співвідношення сторін 1:1
+                          ),
+                          itemBuilder: (context, dotIndex) {
+                            return SizedBox(
+                              // width: 10, // Збільшуємо розмір кружечків
+                              // height: 10,
+                              child: buildDot(
+                                dotIndex,
+                                start,
+                                knownWords,
+                                learnWords,
+                                context,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
