146bf8662b5143a309a629ab689d73f106a1f84f
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-07-17 14:27:54 +0300

    ScreenUtilInit for adaptation home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:words/screens/settiings/auth.dart';
 import 'package:words/screens/category.dart';
 import 'package:words/screens/folder_list.dart';
 import 'package:words/screens/word_group.dart';
 import 'package:words/utils/show_settings.dart';
-import 'package:words/screens/game_screen.dart'; 
+import 'package:words/screens/game_screen.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -33,181 +34,186 @@ class HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: IconButton(
-            icon: Icon(Icons.menu,
-                color: Theme.of(context).colorScheme.onSecondary),
-            onPressed: () {
-              showSettingsScreen(context);
-            },
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) {
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.background,
+            elevation: 0,
+            leading: Padding(
+              padding: EdgeInsets.only(left: 20.w),
+              child: IconButton(
+                icon: Icon(Icons.menu,
+                    color: Theme.of(context).colorScheme.onSecondary),
+                onPressed: () {
+                  showSettingsScreen(context);
+                },
+              ),
+            ),
           ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 30),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'All',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSecondary,
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          height: 0.9,
+          body: Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30.h),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'All',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontSize: 50.sp,
+                              fontWeight: FontWeight.bold,
+                              height: 0.9,
+                            ),
+                          ),
+                          Text(
+                            'words',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.onSecondary,
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                              height: 0.9,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 250.w,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const WordGroupScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.r),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 25.h),
+                        shadowColor: Colors.black,
+                        elevation: 10,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Discover',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 21.sp,
+                          ),
                         ),
                       ),
-                      Text(
-                        'words',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onSecondary,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          height: 0.9,
+                    ),
+                  ),
+                  SizedBox(height: 30.h),
+                  SizedBox(
+                    width: 250.w,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        _navigateToCategories(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.r),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 25.h),
+                        shadowColor: Colors.black,
+                        elevation: 10,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Categories',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 21.sp,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const WordGroupScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    shadowColor: Colors.black,
-                    elevation: 10,
                   ),
-                  child: const Center(
-                    child: Text(
-                      'Discover',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
+                  SizedBox(height: 30.h),
+                  SizedBox(
+                    width: 250.w,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const FolderListScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.r),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 25.h),
+                        shadowColor: Colors.black,
+                        elevation: 10,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'My folders',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 21.sp,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: () {
-                    _navigateToCategories(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    shadowColor: Colors.black,
-                    elevation: 10,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Categories',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
+                  SizedBox(height: 30.h),
+                  SizedBox(
+                    width: 250.w,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const GameScreen()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.r),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 25.h),
+                        shadowColor: Colors.black,
+                        elevation: 10,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Play Game',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 21.sp,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  const Expanded(child: SizedBox()),
+                ],
               ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const FolderListScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    shadowColor: Colors.black,
-                    elevation: 10,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'My folders',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const GameScreen()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    shadowColor: Colors.black,
-                    elevation: 10,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Play Game',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 21,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const Expanded(child: SizedBox()),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
