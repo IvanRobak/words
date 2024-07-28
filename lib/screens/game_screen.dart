2fce401915a8ec7c991bacdcd139de9beaeef0c3
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-07-28 12:32:46 +0300

    GameScreen

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:words/screens/guess_word_screen.dart';
 import 'package:words/screens/guess_image_screen.dart';
 
@@ -28,25 +29,60 @@ class GameScreenState extends State<GameScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const GuessWordScreen()),
-                );
-              },
-              child: const Text('Guess the Word'),
+            SizedBox(
+              width: 250.w,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GuessWordScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.surface,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.r),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 20.h),
+                  shadowColor: Colors.black,
+                  elevation: 10,
+                ),
+                child: Text(
+                  'Guess the Word',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                  ),
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const GuessImageScreen()),
-                );
-              },
-              child: const Text('Guess the Image'),
+            SizedBox(height: 30.h),
+            SizedBox(
+              width: 250.w,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GuessImageScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.surface,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.r),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 20.h),
+                  shadowColor: Colors.black,
+                  elevation: 10,
+                ),
+                child: Text(
+                  'Guess the Image',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
