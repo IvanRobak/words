557a15ddb2def3738df83dba56c42892f6e45f7f
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-08-10 17:54:27 +0300

    filteredWords  in FolderContentScreen

--- lib/screens/folders/folder_content_screen.dart
+++ lib/screens/folders/folder_content_screen.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:words/models/folder.dart';
-import 'package:words/widgets/folder_button.dart';
+import 'package:words/providers/word_provider.dart';
+import 'package:words/widgets/word_list_view.dart';
 
-class FolderContentScreen extends StatelessWidget {
+class FolderContentScreen extends ConsumerStatefulWidget {
   final Folder folder;
 
   const FolderContentScreen({super.key, required this.folder});
 
   @override
+  ConsumerState<FolderContentScreen> createState() =>
+      _FolderContentScreenState();
+}
+
+class _FolderContentScreenState extends ConsumerState<FolderContentScreen> {
+  TextEditingController searchController = TextEditingController();
+  int columns = 2;
+  final List<int> columnOptions = [2, 3];
+  late FocusNode searchFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    searchFocusNode = FocusNode();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      loadFolderWords();
+    });
+  }
+
+  void loadFolderWords() {
+    ref.read(wordFilterProvider.notifier).setWords(widget.folder.words);
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    int columns = 2;
+    final filteredWords = ref.watch(wordFilterProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          folder.name,
+          widget.folder.name,
           style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
         ),
         iconTheme: IconThemeData(
@@ -22,31 +55,27 @@ class FolderContentScreen extends StatelessWidget {
         ),
         backgroundColor: Theme.of(context).colorScheme.surface,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: columns,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 3,
-                ),
-                itemCount: folder.words.length,
-                itemBuilder: (context, index) {
-                  final word = folder.words[index];
-                  return FolderButton(
-                    word: word,
-                    columns: columns,
-                    words: folder.words,
-                    index: index,
-                  );
-                },
-              ),
-            ),
-          ],
+      body: GestureDetector(
+        onTap: () {
+          searchFocusNode.unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: WordListView(
+            words: filteredWords,
+            columns: columns,
+            columnOptions: columnOptions,
+            searchController: searchController,
+            onColumnsChanged: (int? newValue) {
+              setState(() {
+                columns = newValue!;
+              });
+            },
+            onSearchChanged: (query) {
+              ref.read(wordFilterProvider.notifier).filterWords(query);
+            },
+            searchFocusNode: searchFocusNode,
+          ),
         ),
       ),
     );
