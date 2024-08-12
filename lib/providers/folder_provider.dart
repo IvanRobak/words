51bfb8cf5dc112b7f76f8341e61d0606b957a64b
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-08-12 22:21:03 +0300

    _saveFoldersToPrefs

--- lib/providers/folder_provider.dart
+++ lib/providers/folder_provider.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:words/models/folder.dart';
+import 'dart:convert';
+
 import 'package:words/models/word.dart';
 
 class FolderNotifier extends ChangeNotifier {
   final List<Folder> _folders = [
-    Folder(name: 'Clothes', words: [], color: Colors.indigo),
-    Folder(name: 'Animals', words: [], color: Colors.green),
-    Folder(name: 'Fruits', words: [], color: Colors.red),
+    Folder(name: 'Exam', words: [], color: Colors.indigo),
+    Folder(name: 'Home', words: [], color: Colors.green),
+    Folder(name: 'Basic', words: [], color: Colors.red),
   ];
+
+  FolderNotifier() {
+    _loadFoldersFromPrefs();
+  }
 
   List<Folder> get folders => _folders;
 
   void addFolder(String name, Color color) {
     _folders.add(Folder(name: name, words: [], color: color));
+    _saveFoldersToPrefs();
     notifyListeners();
   }
 
   void updateFolderName(int index, String name) {
     _folders[index] = Folder(
         name: name, words: folders[index].words, color: folders[index].color);
+    _saveFoldersToPrefs();
     notifyListeners();
   }
 
   void updateFolderColor(int index, Color color) {
     _folders[index] = Folder(
         name: folders[index].name, words: folders[index].words, color: color);
+    _saveFoldersToPrefs();
     notifyListeners();
   }
 
   void deleteFolder(int index) {
     _folders.removeAt(index);
+    _saveFoldersToPrefs();
     notifyListeners();
   }
 
@@ -44,6 +55,7 @@ class FolderNotifier extends ChangeNotifier {
     for (var folder in _folders) {
       if (folder.name == folderName) {
         folder.addWord(word);
+        _saveFoldersToPrefs();
         notifyListeners();
         break;
       }
@@ -54,6 +66,7 @@ class FolderNotifier extends ChangeNotifier {
     for (var folder in _folders) {
       if (folder.name == folderName) {
         folder.removeWord(word);
+        _saveFoldersToPrefs();
         notifyListeners();
         break;
       }
@@ -63,6 +76,41 @@ class FolderNotifier extends ChangeNotifier {
   void removeWordFromAllFolders(Word word) {
     for (var folder in _folders) {
       folder.removeWord(word);
+    }
+    _saveFoldersToPrefs();
+    notifyListeners();
+  }
+
+  Future<void> _saveFoldersToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<String> folderList = _folders.map((folder) {
+      return jsonEncode({
+        'name': folder.name,
+        'color': folder.color.value,
+        'words': folder.words.map((word) => word.toJson()).toList(),
+      });
+    }).toList();
+    await prefs.setStringList('folders', folderList);
+  }
+
+  Future<void> _loadFoldersFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<String>? folderList = prefs.getStringList('folders');
+
+    if (folderList == null || folderList.isEmpty) {
+      await _saveFoldersToPrefs();
+    } else {
+      _folders.clear();
+      _folders.addAll(folderList.map((folderStr) {
+        final Map<String, dynamic> folderMap = jsonDecode(folderStr);
+        return Folder(
+          name: folderMap['name'],
+          color: Color(folderMap['color']),
+          words: (folderMap['words'] as List<dynamic>)
+              .map((wordMap) => Word.fromJson(wordMap))
+              .toList(),
+        );
+      }).toList());
     }
     notifyListeners();
   }
