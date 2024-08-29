79d1c0d16818d0a7825eeca0f2f89f168ba4a062
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2024-08-29 12:13:43 +0300

    clearProgress

--- lib/providers/correct_answer_provider.dart
+++ lib/providers/correct_answer_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WordProgressNotifier extends StateNotifier<Map<int, double>> {
   WordProgressNotifier() : super({});
@@ -8,14 +11,31 @@ class WordProgressNotifier extends StateNotifier<Map<int, double>> {
       ...state,
       wordId: (state[wordId] ?? 0.0) + 0.2,
     };
+    _saveProgress();
   }
 
   double getProgress(int wordId) {
     return state[wordId] ?? 0.0;
   }
+
+  void clearProgress() {
+    state = {};
+    _saveProgress(); // Зберегти очищений прогрес
+  }
+
+  Future<void> loadProgress() async {
+    final prefs = await SharedPreferences.getInstance();
+    final progressData = prefs.getString('wordProgress') ?? '{}';
+    state = Map<int, double>.from(json.decode(progressData));
+  }
+
+  Future<void> _saveProgress() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('wordProgress', json.encode(state));
+  }
 }
 
 final wordProgressProvider =
     StateNotifierProvider<WordProgressNotifier, Map<int, double>>((ref) {
-  return WordProgressNotifier();
+  return WordProgressNotifier()..loadProgress();
 });
--- lib/screens/settiings/settings_screen.dart
+++ lib/screens/settiings/settings_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:words/providers/button_provider.dart';
+import 'package:words/providers/correct_answer_provider.dart';
 import 'package:words/providers/favorite_provider.dart';
 import 'package:words/screens/settiings/about_screen.dart';
 import 'package:words/screens/settiings/auth_screen.dart';
@@ -83,6 +84,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     ref.read(knownWordsProvider.notifier).loadKnownWords();
     ref.read(learnWordsProvider.notifier).loadLearnWords();
     ref.read(favoriteProvider.notifier).clearFavorites();
+    ref.read(wordProgressProvider.notifier).clearProgress();
 
     final BuildContext currentContext = context;
 
